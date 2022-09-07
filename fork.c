941be62e28ba7362bff9a3095defe6a9e300c7e1
dani198 <danyleul16@gmail.com>
2022-09-07 11:46:41 -0700

    shell

--- fork.c
+++ fork.c
@@ -8,22 +8,16 @@
 int main(void)
 {
 pid_t my_pid;
-pid_t child_pid;
-child_pid = fork();
-if (child_pid == -1)
+pid_t pid;
+printf("Before fork\n");
+pid = fork();
+if (pid == -1)
 {
 perror("Error:");
 return (1);
 }
+printf("After fork\n");
 my_pid = getpid();
 printf("My pid is %u\n", my_pid);
-if (child_pid == 0)
-{
-printf("(%u) Nooooooooo!\n", my_pid);
-}
-else
-{
-printf("(%u) %u, I am your father\n", my_pid, child_pid);
-}
 return (0);
 }
