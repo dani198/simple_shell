0de4c5f474b7dc228b692f0e9307c3073c58fa63
dani198 <danyleul16@gmail.com>
2022-09-08 07:28:08 +0300

    modification on main

--- main.h
+++ main.h
@@ -1,32 +1,39 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#include <sys/wait.h>
-#include <sys/types.h>
+
+#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 #include <unistd.h>
-#include <dirent.h>
-#include <limits.h>
-#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <signal.h>
 extern char **environ;
-char *show_input(void);
+extern int dircount;
+#define DELIM " \n\t"
+
+void env(char **env);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
 void prompt(void);
-char *_strcat(char *src);
-int _strlen(char *str);
-void place(char *str);
-char *findfile(char *command);
-char *find_command(char *command);
-int compare(char *s1, char *s2);
-int _strcmpdir(char *s1, char *s2);
-int charput(char c);
-void place(char *str);
-char *str_concat(char *s1, char *s2);
-int lookforslash(char *cmd);
-int compareExit(char *s1, char *s2);
-int compareEnv(char *s1, char *s2);
-void execute_proc(char **cmd);
-char **identify_string(char *parameter);
-void controlC(int sig);
+char *get_line(void);
+char **split_line(char *line);
+char *get_env(char **env);
+char *pathCat(char *dir, char *av);
+char **dirTok(char **env);
+void loop(char **env);
+char *checkPath(char **dir, char *command);
+int execute(char *fullPath, char **command);
+int exit_sh(char **command);
+int cd(char **command);
+int printenv(char **command);
+int checkBuiltins(char *combine, char **command);
+void handler(int sig);
+void buffers1(char *line, char **command);
+void buffers2(char **dir, char *combine);
+void buffers3(char **tokens, char *buf);
+void buffers4(char **tok, char *buf2);
+void buffers5(char *dup);
 #endif
